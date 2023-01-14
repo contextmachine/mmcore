0a8ede08febf3755b8253b0545eaa1569ae5e904
sth-v <aa@contextmachine.online>
2023-01-14 06:16:38 +0300

    large update
    Signed-off-by: sth-v <aa@contextmachine.online>

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ print("""
 Copyright (c) 2022. CONTEXTMACHINE. AEC. 
 Computational Geometry, Digital Engineering and Optimizing your construction process.    
 ------------------------------------------------------------------------------------------------------------------------    
-                                                               
+
        ____  ____ ___  _/ /__ ____ __  __ / /__ ______    ___ __ ____ / /_  __ ___    ____     
      / ,___/ __  / __ \  ___ /  ___\ \/  /  __ / __  __ \/ __  / ,___/ __ \/ / __ \/  ___/       
     / /___/ /_/ / / / / /___/  ___ /    / /___/ / / / / / /_/ / /___/ / / / / / / /  ___/     
     \____/\___ /_/ /_/\____/\____ /_ /\_\____/_/ /_/ /_/\__,_/\____/_/ /_/_/_/ /_/\____/       
-                                                                                                      
-                                                                                                      
+
+
 Andrew Astkhov (sth-v) aa@contextmachine.ru 
 🔫📠️💻🎚📹📟🚔🦾🦺📠️💻🎛️📱🚧🚔🦾🔫📠️🛠️🎛️📹📟🚔🦾🔫📠️👷‍🎛️🔩📟🚜⚙️🔫📠️💻🎚📹📟🚔🦾🦺📠️💻🎛️📱🚧🚔🦾🔫📠️🛠️🎛️📹📟🚔🦾🔫📠️👷‍🎛️🔩📟🚜⚙️🔫📠️💻🎚📹📟🚔🦾                                                          
 ------------------------------------------------------------------------------------------------------------------------
@@ -19,10 +19,11 @@ Andrew Astkhov (sth-v) aa@contextmachine.ru
       )
 setup(
     name='mmcore',
-    version='0.1.1',
+    version='0.1.2',
     packages=['mmcore', 'mmcore.geom', 'mmcore.geom.tools', 'mmcore.geom.xforms', 'mmcore.geom.parametric',
               'mmcore.utils', 'mmcore.utils.sockets', 'mmcore.utils.versioning', 'mmcore.utils.pydantic_mm',
               'mmcore.utils.redis_tools', 'mmcore.addons', 'mmcore.addons.gmdl', 'mmcore.addons.opencascade',
+              'mmcore.addons.rhino', 'mmcore.addons.rhino.compute', 'mmcore.addons.rhino.native', 'mmcore.services',
               'mmcore.baseitems', 'mmcore.baseitems.descriptors', 'mmcore.collection', 'mmcore.collection.generics',
               'mmcore.exceptions'],
     url='',
@@ -30,4 +31,4 @@ setup(
     author='sth-v',
     author_email='aa@contextmachine.ru',
     description=''
-)
+    )
