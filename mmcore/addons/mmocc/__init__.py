9ae6dab149fafd991bb9766e404e13877cbd19ef
sth-v <aa@contextmachine.online>
2023-01-17 18:47:09 +0300

    occ utils added update
    Signed-off-by: sth-v <aa@contextmachine.online>

--- mmcore/addons/mmocc/__init__.py
+++ mmcore/addons/mmocc/__init__.py
@@ -1,1 +1,5 @@
 #  Copyright (c) 2022. Computational Geometry, Digital Engineering and Optimizing your construction processe"
+import sys
+
+sys.path.append('pythonocc-utils')
+sys.path.append('OCCUtils')
