bddf13473046647bd0e3e58e24c718962cb373f2
sth-v <aa@contextmachine.online>
2023-01-17 18:47:09 +0300

    occ utils added update
    Signed-off-by: sth-v <aa@contextmachine.online>

--- mmcore/addons/rhino/__init__.py
+++ mmcore/addons/rhino/__init__.py
@@ -1,8 +1,4 @@
 # Native imports
-import json
 
 from mmcore.addons.rhino.native import random
 from mmcore.addons.rhino.native.utils import *
-
-with open("mmconfig.json") as config_file:
-    mmconfig = json.load(config_file)
