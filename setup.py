c5e6019694a609a23057cc74baec9347ef20f04a
sth-v <aa@contextmachine.online>
2023-03-17 08:15:29 +0300

    tt

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
               'mmcore.addons.rhino',
               'mmcore.addons.rhino.compute',
               'mmcore.addons.rhino.native',
+              'mmcore.addons.rhino.random',
               'mmcore.addons.mmocc',
               'mmcore.addons.mmocc.OCCUtils',
               'mmcore.services',
