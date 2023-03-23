e17d02e5db3e292ae81d17351f43ed6adf4b4de9
sth-v <aa@contextmachine.online>
2023-03-23 10:56:20 +0300

    Dockerfile
    Signed-off-by: sth-v <aa@contextmachine.online>

--- buildme.sh
+++ buildme.sh
@@ -1,1 +1,2 @@
 DOCKER_BUILDKIT=1 docker build --no-cache --platform amd64 -t sthv/mmcore:amd64 . && docker run --name mmcore -t -i -e ./.env -w /mmcore -P --privileged sthv/mmcore:amd64
+docker push -t  sthv/mmcore:amd64
