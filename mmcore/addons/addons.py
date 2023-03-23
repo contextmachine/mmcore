3b11665e72799fe6c781bd7808b9fce5e5d5b474
sth-v <aa@contextmachine.online>
2023-03-23 08:04:01 +0300

    Dockerfile
    Signed-off-by: sth-v <aa@contextmachine.online>

--- mmcore/addons/addons.py
+++ mmcore/addons/addons.py
@@ -3,40 +3,51 @@ from typing import ContextManager
 
 import dotenv
 
+from mmcore.collections.multi_description import ES
 from mmcore.gql.client import GQLReducedQuery
-from mmcore.services.client import get_connection, get_connection_by_host_port
-
+from mmcore.services.client import get_connection_by_host_port
 
 hosts = ["localhost", "host.docker.internal"]
+import itertools
+
+
+def resolve_query(data):
+    lst = []
+    port = []
+
+    for i, v in enumerate(ES(data)["serviceByTopic"]._seq):
+        print(i, v
+              )
+        if v["attributes"] is not None:
+            port = v["attributes"]["port"]
+            *itr, = itertools.zip_longest(ES(v['hosts'])["host"]["address"], [port], fillvalue=port)
+            lst.extend(itr)
+
+    return lst
 
 
 class ModuleResolver(ContextManager):
     def __init__(self, dotenv_name=".env"):
         self.env = dotenv.dotenv_values(dotenv.find_dotenv(dotenv_name, usecwd=True))
         self.query = GQLReducedQuery("""
-        
 query MyQuery($value: String!) {
-  platform_topics_by_pk(value: $value) {
-    value
-    subscribes {
-      serviceByTopic {
-        attributes {
-          port
-        }
-        hosts {
-          host {
-            name
-            address
+    platform_topics_by_pk(value: $value) {
+           
+        subscribes {
+            serviceByTopic {
+                attributes {
+                  port
+                }
+            hosts {
+                host {
+                name
+                    address 
+                  }
+                }
+              }
+            }
           }
-        }
-      }
-    }
-  }
-}
-
-
-        """)
-
+        }""")
     def __enter__(self):
         print("__enter__", self)
         return self
@@ -46,13 +57,9 @@ query MyQuery($value: String!) {
         self.exc_type, self.exc_val, self.exc_tb = exc_type, exc_val, exc_tb
         if exc_val:
             print("__exit__2", exc_val)
-            # self.exc_response=self.query(variables={"_eq1": self.exc_val.name.replace(".", "")})
-            resp=self.query(variables={"value": self.exc_val.name})["subscribes"][0]["serviceByTopic"]
+            response = resolve_query(self.query(variables={"value": self.exc_val.name.replace(".", "")})['subscribes'])
 
-            port = resp["attributes"]["port"]
-            hosts = resp["hosts"]["port"]
-
-            self.conn = get_connection_by_host_port(itertools.zip_longest(hosts, [port], fillvalue=port))
+            self.conn = get_connection_by_host_port(*response)
             missed_module = self.conn.root.getmodule(self.exc_val.name)
 
             sys.modules[self.exc_val.name] = missed_module
