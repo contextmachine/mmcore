f633c89ed15cce5f26881a3a101c1ccffe40e4cd
sth-v <aa@contextmachine.online>
2023-03-24 17:10:49 +0300

    changes

--- buildme.sh
+++ buildme.sh
@@ -1,4 +1,2 @@
 DOCKER_BUILDKIT=1 docker build --no-cache --platform amd64 -t sthv/mmcore:amd64 . && docker run --name mmcore -t -i -e ./.env -w /mmcore -P --privileged sthv/mmcore:amd64
 docker tag sthv/mmcore:amd64 cr.yandex/crpfskvn79g5ht8njq0k/mmcore:amd64
-docker push cr.yandex/crpfskvn79g5ht8njq0k/mmcore:amd64
-docker push cr.yandex/crpfskvn79g5ht8njq0k/mmcore:amd64
