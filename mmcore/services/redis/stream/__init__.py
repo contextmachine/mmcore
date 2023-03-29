640c85a78ef286d6bc9c757b2124288a00f5732a
sth-v <aa@contextmachine.online>
2023-03-29 18:59:31 +0300

    docker
    Signed-off-by: sth-v <aa@contextmachine.online>

--- mmcore/services/redis/stream/__init__.py
+++ mmcore/services/redis/stream/__init__.py
@@ -1,5 +1,4 @@
 import redis
-import redis_om
 
 
 class StreamConnector:
@@ -11,16 +10,15 @@ class StreamConnector:
         self.__dict__ |= kwargs
         self.name = name
         self.conn = redis_conn
-        self.last_pub=None
+        self.last_pub = None
         self.last_sub = None
-        redis_om.get_redis_connection()
 
     def write_to_stream(self, data, **kwargs):
         last_pub_id = self.xadd(self.name, data, **kwargs)
         return last_pub_id
 
-    def read_from_stream(self, data, min="-", max="+", count=1, **kwargs):
-        self.last_sub = self.xrevrange( min=min, max=max, count=count, **kwargs)
+    def read_from_stream(self, min="-", max="+", count=1, **kwargs):
+        self.last_sub = self.xrevrange(min=min, max=max, count=count, **kwargs)
 
     def create_group(self, group_name=None, id="*", **kwargs):
         group = self.group if group_name is None else group_name
@@ -50,12 +48,87 @@ class StreamConnector:
         self.conn.xlen(self.name)
 
     def __getitem__(self, item):
-        min=str(int(item))
-        max=str(int(item) + 1)
+        min = str(int(item))
+        max = str(int(item) + 1)
 
         return self.xrange(count=1, min=min, max=max)
+
     def keys(self):
         return [tg.decode().split("-")[0] for tg in list(zip(*self.xrange(count=None)))[0]]
 
     def __len__(self):
         return self.xlen()
+
+
+import json
+
+
+def encode_dict_with_bytes(obj):
+    if isinstance(obj, dict):
+        return dict((encode_dict_with_bytes(k), encode_dict_with_bytes(v)) for k, v in obj.items())
+    elif isinstance(obj, (list, tuple)):
+        return [encode_dict_with_bytes(v) for v in obj]
+    elif isinstance(obj, (bytes, bytearray)):
+        norm = obj.decode("ASCII")
+        print(norm)
+        if norm == '':
+            return ''
+        else:
+            if all([char in "0123456789." for char in norm]):
+                if "." in norm:
+                    return float(norm)
+                else:
+                    return int(norm)
+            elif '"' in norm:
+                return json.loads(obj.decode("ASCII"))
+            else:
+                return obj.decode("ASCII")
+    else:
+        return obj
+
+
+class ThreeCommit(dict):
+    _stream_bane = "api:stream:test"
+    schema = {
+        "object": {
+
+        },
+        "materials": [
+
+        ],
+        "geometries": [
+
+        ],
+        "metadata": {
+
+        }
+    }
+
+    def __init__(self, name, conn, **kwargs):
+        super().__init__(**kwargs)
+        self.stream_name = name
+        self.conn = conn
+
+    def last(self):
+        return encode_dict_with_bytes(self.stream.get_last())
+
+    @property
+    def stream_name(self):
+        return self._stream_name
+
+    @stream_name.setter
+    def stream_name(self, v):
+        self._stream_name = v
+
+    @property
+    def stream(self):
+        return StreamConnector(self.stream_name, redis_conn=self.conn)
+
+    def commit(self):
+        return self.stream.xadd(None, fields={
+            '"object"': json.dumps(self["object"]),
+            '"materials"': json.dumps(self["materials"]),
+            '"geometries"': json.dumps(self["geometries"]),
+            '"metadata"': json.dumps(self["metadata"])
+        }
+                                )
