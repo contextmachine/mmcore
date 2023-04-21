8cd8dc7342ed398345666bb6a759a972d057df9b
sth-v <aa@contextmachine.online>
2023-04-21 06:10:42 +0300

    Add:  graphql v2 test server  mmcore base classes changes

--- mmcore/gql/api/main.py
+++ mmcore/gql/api/main.py
@@ -1,17 +1,8 @@
-import json
+import dataclasses
 import typing
-
-import fastapi
+from mmcore.gql.api.parse import parse_simple_query
 import uvicorn
 from fastapi import FastAPI
-
-import dataclasses
-
-gqlapp=FastAPI()
-@dataclasses.dataclass
-class Query:
-    query:str
-    variables: dict[str, typing.Any] | dict[None, None]
 
 data_test2 = {
     'test': {
@@ -36,22 +27,21 @@ data_test2 = {
     }
 }
 
-# qt.interpret()(data_test)
+
+@dataclasses.dataclass
+class Query:
+    query: str
+    variables: dict[str, typing.Any] | dict[None, None]
 
 
+graphql_app = FastAPI()
 
 
-from mmcore.gql.api.parse import parse_simple_query
-
-
-
-@gqlapp.post("/v2/qraphql")
-def qraphql2(data: Query):
-
+@graphql_app.post("/v2/graphql")
+def graphql2(data: Query):
     qt2 = parse_simple_query(data.query)
-
     return qt2.resolve(data_test2)
 
-if __name__ == "__main__":
 
-    uvicorn.run("main:gqlapp", host="0.0.0.0", port=5799)
+if __name__ == "__main__":
+    uvicorn.run("main:graphql_app", host="0.0.0.0", port=5799)
