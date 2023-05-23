b72da9abfcd14de845860f73362d32e0b1be0780
sth-v <aa@contextmachine.online>
2023-05-23 17:25:47 +0300

    update version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ Andrew Astkhov (sth-v) aa@contextmachine.ru
 
 setup(
     name='mmcore',
-    version='0.1.7',
+    version='0.1.9',
     packages=['mmcore','mmcore.mmbuild', 'mmcore.geom',
               'mmcore.geom.utils',
               'mmcore.geom.transform',
