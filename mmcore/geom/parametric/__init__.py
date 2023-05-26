fa5d91f8d3aea6276a22739d69b535a6dc7b4126
sth-v <aa@contextmachine.online>
2023-05-27 00:26:27 +0300

    add circle example

--- mmcore/geom/parametric/__init__.py
+++ mmcore/geom/parametric/__init__.py
@@ -1,6 +1,5 @@
 #  Copyright (c) 2022. Computational Geometry, Digital Engineering and Optimizing your construction processe"
-import copy
-from abc import ABC, ABCMeta, abstractmethod
+
 from functools import wraps
 from mmcore.geom.parametric.sketch import *
 import compas.geometry as cg
