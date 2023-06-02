ee60de9bf8ad9633f1a8743a3f339a4f0702b1d7
Sofya Dobychina <96266344+sf-d@users.noreply.github.com>
2023-06-02 19:17:57 +0300

    add_profile

--- lht20/podsistema.py
+++ lht20/podsistema.py
@@ -8,16 +8,24 @@ from mmcore.base.sharedstate import serve
 HIGH = 10
 with open("examples/data/profile.json") as f:
     profile = json.load(f)
+
+with open("examples/data/artem_profile.json") as f:
+    artem_profile = json.load(f)
+
+
+
 shape = Shape(boundary=profile["bounds"], holes=profile["holes"])
 extrusion = simple_extrusion(shape, HIGH)
 
-rot_extrusion = simple_extrusion(shape, HIGH)
-rot_extrusion.rotate(np.pi / 2, [1, 0, 0])
+shape_profile = Shape(boundary=artem_profile["bounds"], holes=artem_profile["holes"])
 
-def bim_sequence_generator(extr=extrusion):
+extr_ver = simple_extrusion(shape_profile, HIGH)
+extr_ver.rotate(np.pi / 2, [1, 0, 0])
+
+def bim_sequence_generator(extr=extrusion, transl=(1, 0, 0)):
     for i in range(10):
         next_extrusion = extr.__copy__()
-        next_extrusion.translate([1, 0, 0])
+        next_extrusion.translate(transl)
         yield next_extrusion
 
 if __name__ == "__main__":
@@ -26,8 +34,9 @@ if __name__ == "__main__":
 
     for bim in bim_sequence_generator(extrusion):
         group.add(bim)
-    for bim in bim_sequence_generator(rot_extrusion):
+    for bim in bim_sequence_generator(extr_ver, transl=(-1, 0, 0)):
         group.add(bim)
+
     with open("model.json", "w") as f:
         json.dump(group.root(), f)  # now you can view it with three js
     serve.start_as_main()
