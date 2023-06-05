fa1add0e135b0b250394e92a0bac672f81336960
Andrew Astakhov <aa@contextmachine.ru>
2023-06-06 01:49:40 +0300

    Delete mmcore/addons/mmocc directory
    Signed-off-by: Andrew Astakhov <aa@contextmachine.ru>

--- mmcore/addons/mmocc/__init__.py
+++
@@ -1,9 +0,0 @@
-#  Copyright (c) 2022. Computational Geometry, Digital Engineering and Optimizing your construction processe"
-import os
-import posixpath
-import sys
-
-PWD = os.getenv("PWD")
-posixpath.abspath("mmcore/")
-
-sys.path.append('OCCUtils')
