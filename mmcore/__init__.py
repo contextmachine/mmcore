8d45bacd582375c18d38cc8ee29fdfef9ca1a3ed
sth-v <aa@contextmachine.online>
2023-06-07 03:33:41 +0300

    verse-upd

--- mmcore/__init__.py
+++ mmcore/__init__.py
@@ -1,5 +1,5 @@
 import dotenv
-
+import os
 TOLERANCE = 0.000_001
 def load_dotenv_from_path(filename, override=False, raise_error_if_not_found=False, **kwargs):
     return dotenv.load_dotenv(dotenv_path=dotenv.find_dotenv(filename=filename,
@@ -12,7 +12,8 @@ def load_dotenv_from_stream(stream, override=False, **kwargs):
 
 
 def __version__():
-    return "0.2.7"
+    with open(".version") as f:
+        return f.read()
 
 
 load_dotenv_from_path(".env")
