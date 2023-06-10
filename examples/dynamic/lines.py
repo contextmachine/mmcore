5867c1b31996b884bc795b8f5107d2c76a6d6e6f
sth-v <aa@contextmachine.online>
2023-06-10 15:21:05 +0300

    params added

--- examples/dynamic/lines.py
+++ examples/dynamic/lines.py
@@ -1,4 +1,3 @@
-
 import time
 import json
 
@@ -9,8 +8,7 @@ from mmcore.geom.parametric import Linear
 # This Api provides an extremely flexible way of updating data. You can pass any part of the parameter dictionary
 # structure, the parameters will be updated recursively and only the part of the graph affected by the change
 # will be recalculated.
-from mmcore.base.params import ParamGraphNode, param_graph_node
-
+from mmcore.base.params import ParamGraphNode, param_graph_node, param_graph_node_native
 
 a = ParamGraphNode(dict(x=1.0, y=2.0, z=3.0), name="A")
 b = ParamGraphNode(dict(x=-1.0, y=-2.0, z=-3.0), name="B")
@@ -25,6 +23,14 @@ def line_from_points(start, end):
 @param_graph_node(params=dict(start=a, end=b))
 def first_line_from_points(start, end):
     return line_from_points(start, end)
+
+
+@param_graph_node_native
+def first_line_from_points2(start=a, end=b):
+    return line_from_points(start, end)
+
+
+
 
 
 @param_graph_node(params=dict(start=c, end=d))
@@ -66,7 +72,7 @@ for i, line in enumerate([result_line_from_points, first_line_from_points, secon
                                             uuid=f"line_{i}",
                                             color=colors[i]),
                                        name=f"render_line_{i}",
-                                       resolver=lambda prm: render_line(**prm)))
+                                       resolver=lambda **prm: render_line(**prm)))
 
 
 @param_graph_node(params=dict(result=render_nodes[0], uuid="result_group", secondary_color=(100, 100, 100)))
@@ -174,39 +180,44 @@ def changes1():
         }
     }
     )
-ptA,ptB,ptC,ptD=(-8.323991, 6.70421, -8.323991) ,(-8.323991, -6.70421, 8.323991) ,(8.323991, 6.70421, 8.323991 ) ,(8.323991, -6.70421, -8.323991)
+
+
+ptA, ptB, ptC, ptD = (-8.323991, 6.70421, -8.323991), (-8.323991, -6.70421, 8.323991), (8.323991, 6.70421, 8.323991), (
+    8.323991, -6.70421, -8.323991)
 
 
 def changes2(a=ptA,
              b=ptB,
              c=ptC,
              d=ptD):
-
     render_lines(result={
         "line": {
             "start": {
                 "line": {
                     "start": {"x": a[0], "y": a[1], "z": a[2]},
-                    "end":  {"x": b[0], "y": b[1], "z": b[2]}
+                    "end": {"x": b[0], "y": b[1], "z": b[2]}
                 },
                 "t": 0.5
             },
             "end": {
-                "line": {"start":  {"x": c[0], "y": c[1], "z": c[2]},
-                         "end":  {"x": d[0], "y": d[1], "z": d[2]}},
+                "line": {"start": {"x": c[0], "y": c[1], "z": c[2]},
+                         "end": {"x": d[0], "y": d[1], "z": d[2]}},
                 "t": 0.2
             }
         }
     }
     )
-@param_graph_node(dict(points=(), uuid="test-polyline", color=(150,150,40)))
+
+
+@param_graph_node(dict(points=(), uuid="test-polyline", color=(150, 150, 40)))
 def polyline(points, uuid, color):
     return ALine(geometry=points, uuid=uuid, material=LineBasicMaterial(color=ColorRGB(*color).decimal))
+
 
 def animate():
     import numpy as np
     import time
-    points=[]
+    points = []
     for i in np.linspace([0, 1], [1, 0], num=100):
         time.sleep(0.01)
 
@@ -224,8 +235,8 @@ def animate():
 
         )
         points.append(result_line_from_points().evaluate(i[0]).tolist())
-        polyline(points=points,uuid="polyline" ,color=(200,200,40))
-        
+        polyline(points=points, uuid="polyline", color=(200, 200, 40))
+
     for j in np.linspace([1, 0], [0, 1], num=100):
         time.sleep(0.01)
         render_lines(result={
@@ -239,14 +250,15 @@ def animate():
             },
             "color": (20, int(j[1] * 255), int(j[0] * 255))
         },
-         secondary_color=(200,200,40) )
+            secondary_color=(200, 200, 40))
         points.append(result_line_from_points().evaluate(j[1]).tolist())
-        polyline(points=points,uuid="polyline" ,color=(150,150,40))
+        polyline(points=points, uuid="polyline", color=(150, 150, 40))
     points.append(result_line_from_points().evaluate(0).tolist())
     polyline(points=points)
-    
+
 
 from mmcore.base.sharedstate import serve
+
 serve.start()
 
 changes2()
