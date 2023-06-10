eed208399cfe9451f81cd5e9eca9d0398acc6459
sth-v <aa@contextmachine.online>
2023-06-10 13:54:20 +0300

    params added

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
               'mmcore.geom.utils',
               'mmcore.geom.transform',
               'mmcore.geom.parametric',
-                     'mmcore.geom.shapes',
+              'mmcore.geom.shapes',
               'mmcore.geom.materials',
               'mmcore.geom.kernel',
               'mmcore.geom.vectors',
