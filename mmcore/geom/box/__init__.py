3f6cab20c3373df632b99a6004d9f045103b1b0b
sth-v <aa@contextmachine.online>
2023-12-09 08:54:44 +0300

    Refactor Box class and add intersection functionality
    The `Box` class in the geometry module is restructured and refactored for improved readability and efficiency. A new method `intersect_with_other_box` is introduced to detect intersection with another box. Eliminated stray model imports and commented out old, unused code.

--- mmcore/geom/box/__init__.py
+++ mmcore/geom/box/__init__.py
@@ -1,9 +1,12 @@
+from typing import Any
+
 import numpy as np
 from multipledispatch import dispatch
 
 from mmcore.base import ageomdict
 from mmcore.common.models.fields import FieldMap
 from mmcore.geom.extrusion import extrude_polyline
+from mmcore.geom.intersections import intersect
 from mmcore.geom.line import Line
 from mmcore.geom.mesh import build_mesh_with_buffer, create_mesh_buffer_from_mesh_tuple, union_mesh_simple
 from mmcore.geom.plane import Plane
@@ -12,18 +15,13 @@ from mmcore.geom.vec import cross, dist, unit
 
 
 class Box(Rectangle):
-    """
->>> from mmcore.base.sharedstate import serve
->>> from mmcore.common.viewer import create_group
->>> from mmcore.geom.vec import unit
->>> group= create_group('fff')
->>> r1=Box(u=10,v=20,h=20)
->>> r2=Box(u=30,v=40,h=60)
->>> r2.translate((50,50,0))
->>> r1.rotate(np.pi/4, axis=unit(np.array([1.,1.,1.])), origin=r2.origin)
->>> group.add(r1.to_mesh())
->>> group.add(r2.to_mesh())
->>> serve.start()
+    """A class representing a box.
+
+    Inherits from Rectangle.
+
+    Attributes:
+        h (float): The height of the box.
+        lock (bool): A flag indicating if the box is locked.
     """
 
     def __init__(self, *args, h=3.0, **kwargs):
@@ -36,6 +34,10 @@ class Box(Rectangle):
             FieldMap('u', 'u1'),
             FieldMap('v', 'v1'),
             FieldMap('h', 'h'),
+            FieldMap('x', 'x'),
+            FieldMap('y', 'y'),
+            FieldMap('z', 'z'),
+
             FieldMap('area', 'area', backward_support=False),
             FieldMap('lock', 'lock'),
 
@@ -76,22 +78,6 @@ class Box(Rectangle):
     def sides(self):
         return self.faces[1:-1]
 
-    def _init_mesh(self, **kwargs):
-        _props = dict()
-
-        self.apply_forward(_props)
-        fcs = self.faces
-        self._mesh = build_mesh_with_buffer(
-            union_mesh_simple(rect_to_mesh_vec(np.array(fcs)).reshape((len(fcs), 3)).tolist()),
-            uuid=self.uuid,
-            props=_props, **kwargs
-
-        )
-        self._mesh.owner = self
-
-    def to_mesh(self, **kwargs):
-        self._init_mesh(**kwargs) if self._mesh is None else self.update_mesh()
-        return self._mesh
 
     def apply_forward(self, data):
         for m in self.field_map:
@@ -102,30 +88,49 @@ class Box(Rectangle):
             m.backward(self, data)
         self._dirty = True
 
-    def update_mesh(self):
+    @property
+    def x(self):
+        return self.origin[0]
 
-        self.apply_backward(self._mesh.properties)
-        fcs = self.faces
-        ageomdict[self._mesh._geometry] = create_mesh_buffer_from_mesh_tuple(
-            union_mesh_simple(rect_to_mesh_vec(np.array(fcs)).reshape((len(fcs), 3)).tolist()),
+    @x.setter
+    def x(self, val) -> None:
+        self.origin[0] = val
 
-            uuid=self._mesh.uuid)
+    @property
+    def y(self):
+        return self.origin[1]
 
-        self.apply_forward(self._mesh.properties)
+    @y.setter
+    def y(self, val) -> None:
+        self.origin[1] = val
+
+    @property
+    def z(self):
+        return self.origin[2]
+
+    @z.setter
+    def z(self, val) -> None:
+        self.origin[2] = val
+
+
 
     def transpose(self) -> 'Box':
         """transpose the box. Это сделвет продольными противоположные ребра"""
-        return Box(self.u, self.h, h=self.v, xaxis=self.xaxis, normal=self.yaxis,
+        return Box(float(self.u), float(self.h), h=float(self.v), xaxis=self.xaxis, normal=self.yaxis,
                    origin=self.origin + (self.normal * self.h))
 
     def elongate_ribs(self):
         return (Line.from_ends(pt, pt + (self.normal * self.h)) for pt in self.corners)
 
     @dispatch(Plane)
-    def thickness_trim(self, plane: Plane) -> 'Box':
+    def thickness_trim(self, plane: Plane) -> 'tuple[Box, bool, Any]':
         """
+        Trim with thickness means that the box will be trimmed to the shortest trimmed side.
+        In other words, no point of the box will extend beyond the trim plane.
+
         Подрезка с толщиной означает что бокс будет образан под самую короткую из подрезающихся сторон.
         Иными словами ни одна точка бокса не выйдет за подрезающую плоскость.
+
         :param plane: Trimming plane
         :type plane: <Plane>
         :return: Trimmed box
@@ -143,9 +148,39 @@ class Box(Rectangle):
         return Box(self.ecs_uv.u, self.ecs_uv.v, h=dist(point, line2.start), xaxis=self.xaxis, origin=self.origin,
                    normal=self.normal), intersection, res
 
+    def create_mesh(self):
+        _props = dict()
+        self.apply_forward(_props)
+        fcs = self.faces
+        self._mesh = build_mesh_with_buffer(
+            union_mesh_simple(rect_to_mesh_vec(np.array(fcs)).reshape((len(fcs), 3)).tolist()),
+            uuid=self.uuid,
+            props=_props
+        )
+        self._mesh.owner = self
+
+    def _init_mesh(self):
+        self.create_mesh()
+
+    def update_mesh(self):
+        self.apply_backward(self._mesh.properties)
+        fcs = self.faces
+        ageomdict[self._mesh._geometry] = create_mesh_buffer_from_mesh_tuple(
+            union_mesh_simple(rect_to_mesh_vec(np.array(fcs)).reshape((len(fcs), 3)).tolist()),
+            uuid=self._mesh.uuid)
+        self.apply_forward(self._mesh.properties)
+
+    def to_mesh(self, **kwargs):
+        self.create_mesh() if self._mesh is None else self.update_mesh()
+        return self._mesh
     @dispatch(Line)
-    def thickness_trim(self, line: Line) -> 'Box':
+    def thickness_trim(self, line: Line) -> 'tuple[Box, bool, Any]':
         """
+        This version of the method takes a line that will be converted to a vertical trim plane.
+
+        Trim with thickness means that the box will be trimmed to the shortest trimmed side.
+        In other words, no point of the box will extend beyond the trim plane.
+
         Эта версия метода принимает линию, которая будет преобразована в вертикальную плоскость подрезки.
 
         Подрезка с толщиной означает что бокс будет образан под самую короткую из подрезающихся сторон.
@@ -165,7 +200,51 @@ class Box(Rectangle):
 
         return self.thickness_trim(Plane(np.array([origin, xaxis, yaxis, zaxis])))
 
+    def intersect_with_other_box(self, other_box: 'Box') -> 'Box':
+        """
+        Intersect this box with another box.
+
+        Returns the resulting intersection box or None if no intersection found.
+
+        :param other_box: Other box
+        :type other_box: <Box>
+        :return: Box resulting from intersection or None
+        :rtype: <Box>
+        """
+        # Get the faces (corners) of the current box and the other box
+        faces_current = np.array(self.faces)
+        faces_other = np.array(other_box.faces)
+
+        # Initialize an empty list to store intersection points
+        intersect_points = []
+
+        for face_current in faces_current:
+            for face_other in faces_other:
+                intersect_pts, _ = intersect(face_current, face_other)
+                if intersect_pts is not None:
+                    intersect_points.append(intersect_pts)
+
+        # If no intersection points were found, there is no intersection and return None
+        if not intersect_points:
+            return None
+
+        intersect_points = np.array(intersect_points)
+
+        # Calculate the bounding box of the intersect points and return the corresponding Box
+
+        return intersect_points
+
 
 def unpack_trim_details(res):
+    """
+    Unpacks the trim details.
+
+    :param res: A list of tuples containing trim details.
+    :type res: list
+
+    :return: A tuple containing the unpacked trim details.
+    :rtype: tuple
+
+    """
     w, t, pts = list(zip(*list(zip(*sorted(res, key=lambda x: x[0])))[1]))
     return np.array(w), np.array(t), np.array(pts)
