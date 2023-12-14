53cf7aab666e741601aecf74f450e847ac7aa618
sth-v <aa@contextmachine.ru>
2023-12-14 06:15:04 +0300

    fix variable offset

--- mmcore/numeric/__init__.py
+++ mmcore/numeric/__init__.py
@@ -105,16 +105,16 @@ def simulated_annealing(objective, bounds, n_iterations, step_size, temp):
 
     """
 
-    best = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
+    best = bounds[:, 0] + np.random.random(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
     # evaluate the initial point
     best_eval = objective(best)
     # current working solution
     curr, curr_eval = best, best_eval
-    scores = list()
+    scores = np.zeros([n_iterations, len(bounds)])
     # run the algorithm
     for i in range(n_iterations):
         # take a step
-        candidate = curr + randn(len(bounds)) * step_size
+        candidate = curr + np.random.randn(len(bounds)) * step_size
         # evaluate candidate point
         candidate_eval = objective(candidate)
         # check for new best solution
@@ -122,7 +122,7 @@ def simulated_annealing(objective, bounds, n_iterations, step_size, temp):
             # store new best point
             best, best_eval = candidate, candidate_eval
             # keep track of scores
-            scores.append(best_eval)
+            scores[i] = best_eval
             # report progress
             print('>%d f(%s) = %.5f' % (i, best, best_eval))
         # difference between candidate and current point evaluation
@@ -130,9 +130,9 @@ def simulated_annealing(objective, bounds, n_iterations, step_size, temp):
         # calculate temperature for current epoch
         t = temp / float(i + 1)
         # calculate metropolis acceptance criterion
-        metropolis = exp(-diff / t)
+        metropolis = np.exp(-diff / t)
         # check if we should keep the new point
-        if diff < 0 or rand() < metropolis:
+        if diff < 0 or np.random.random() < metropolis:
             # store the new current point
             curr, curr_eval = candidate, candidate_eval
-    return [best, best_eval, scores]
+    return best, best_eval, scores
