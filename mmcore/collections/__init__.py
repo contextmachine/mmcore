661dadd325a46bbbaf05c2e762438b5abfa69e7c
sth-v <aa@contextmachine.ru>
2023-12-17 01:22:20 +0300

    docs

--- mmcore/collections/__init__.py
+++ mmcore/collections/__init__.py
@@ -1,4 +1,5 @@
 #  Copyright (c) 2022. Computational Geometry, Digital Engineering and Optimizing your construction processe"
+from __future__ import absolute_import, annotations
 
 
 from mmcore.collections.basic import *
