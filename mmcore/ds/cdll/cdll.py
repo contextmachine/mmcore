85093a5c60559e526b2e24deb9417a742990e610
sth-v <aa@contextmachine.ru>
2023-12-21 08:22:52 +0300

    Remove mmcore/geom, mmcore/interfaces modules and their files
    This commit removes the unneeded modules mmcore/geom/kernel, mmcore/geom/occ, mmcore/geom/reflection, mmcore/interfaces/todict, and mmcore/interfaces. The files within these directories have also been deleted. This is a part of overall restructuring plan to make the project more maintainable and organized. Additional updates in the project structure will follow soon.

--- mmcore/ds/cdll/cdll.py
+++ mmcore/ds/cdll/cdll.py
@@ -2,6 +2,53 @@ from mmcore.ds.cdll.node import Node
 
 
 class CDLL:
+    """
+
+    CDLL
+    ====
+
+    Class representing a Doubly Circular Linked List.
+
+    Methods
+    -------
+
+    __init__()
+        Initializes an empty Doubly Circular Linked List.
+
+    __repr__()
+        Returns a string representation of the Doubly Circular Linked List.
+
+    append(data)
+        Appends a new element at the end of the Doubly Circular Linked List.
+
+    append_node(node)
+        Appends a new node at the end of the Doubly Circular Linked List.
+
+    insert(data, index)
+        Inserts a new element at the specified index in the Doubly Circular Linked List.
+
+    iter_nodes()
+        Returns an iterator that iterates over the nodes in the Doubly Circular Linked List.
+
+    remove(index)
+        Removes the element at the specified index from the Doubly Circular Linked List.
+
+    index(data)
+        Returns the index of the first occurrence of the specified element in the Doubly Circular Linked List.
+
+    get(index)
+        Returns the element at the specified index in the Doubly Circular Linked List.
+
+    size()
+        Returns the number of elements in the Doubly Circular Linked List.
+
+    display()
+        Prints a string representation of the Doubly Circular Linked List.
+
+    get_node(index)
+        Returns the node at the specified index in the Doubly Circular Linked List.
+
+    """
     nodetype = Node
 
     def __init__(self):
@@ -38,25 +85,23 @@ class CDLL:
         return
 
     def insert(self, data, index):
-        if (index > self.count) | (index < 0):
+        if index > self.count or index < 0:
             raise ValueError(f"Index out of range: {index}, size: {self.count}")
-        if self.head == None:
-            self.head = self.nodetype(data)
-            self.count = 1
-            return
-        temp = self.head
-        if (index == 0):
-            temp = temp.previous
+        node = self.nodetype(data)
+        if self.head is None:
+            self.head = node
+            node.next = node
+            node.previous = node
         else:
+            temp = self.head
             for _ in range(index - 1):
                 temp = temp.next
-        temp.next.previous = self.nodetype(data)
-        temp.next.previous.next, temp.next.previous.previous = temp.next, temp
-        temp.next = temp.next.previous
-        if (index == 0):
-            self.head = self.head.previous
+            temp.next.previous = node
+            node.next, node.previous = temp.next, temp
+            temp.next = node
+            if index == 0:
+                self.head = node
         self.count += 1
-        return
 
     def iter_nodes(self):
         if self.head is not None:
