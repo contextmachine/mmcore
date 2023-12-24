57be69d99ced13a7a6a4bf5c425e961749110c87
sth-v <aa@contextmachine.ru>
2023-12-25 02:21:39 +0300

    finalize unit_list.py

--- mmcore/__init__.py
+++ mmcore/__init__.py
@@ -1,12 +1,10 @@
 #Generated with ./bin/upd-version.py
-import numpy as np
-
 from mmcore.utils.env import load_dotenv_from_path, load_dotenv_from_stream
-np.set_printoptions(suppress=True)
+
 load_dotenv_from_path(".env")
 
 TOLERANCE = 0.000_001
 
 
 def __version__():
-    return "0.18.12"
+    return "0.18.13"
