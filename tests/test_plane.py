34d77ad4c15bfe491e85a9515aa66cc43d3e96b7
sth-v <aa@contextmachine.ru>
2024-02-16 12:20:45 +0300

    upd

--- tests/test_plane.py
+++ tests/test_plane.py
@@ -29,12 +29,6 @@ class TestPlane(unittest.TestCase):
         print(res)
         self.assertTrue(all(res))
 
-    def test_timeit_from_normal(self):
-        res = timeit.timeit(
-            "import numpy as np;from mmcore.geom.plane import plane_from_normal_numeric;from tests.test_plane import test_plane_num;test_plane_num(plane_from_normal_numeric(np.random.random(3),np.zeros(3)))",
-            number=1000)
-        print('all:', res, 'per loop:', res / 1000)
-        self.assertLess(res, 0.4)
 
     def test_plane_intersection(self):
         check = Line.from_ends(*np.array([(-14.581811182367993, -10.914653736467775, 1.1467086205536206),
