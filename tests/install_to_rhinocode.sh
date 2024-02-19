44958bd0657bbd8e3c8c00e875bf37c878362113
sth-v <aa@contextmachine.ru>
2024-02-19 21:09:20 +0300

    upd

---
+++ tests/install_to_rhinocode.sh
@@ -0,0 +1,15 @@
+#
+echo "[install_to_rhinocode.sh] Сlearing previous cmmcore versions"
+"$HOME/.rhinocode/py39-rh8/python3.9" -m pip uninstall mmcore -y
+
+
+if [ ! -e $PWD/build ]
+then
+  echo "[install_to_rhinocode.sh] Going to the root of the cmmcore repository root"
+  cd ..
+fi
+echo "[install_to_rhinocode.sh]" "Starting in" "$(pwd)"
+"$HOME/.rhinocode/py39-rh8/python3.9" -m pip install "$(pwd)"
+echo "[install_to_rhinocode.sh] Running tests ... "
+"$HOME/.rhinocode/py39-rh8/python3.9" "$(pwd)/tests/test_intersections.py"
+"$HOME/.rhinocode/py39-rh8/python3.9" "$(pwd)/tests/test_vec_speedups.py"
