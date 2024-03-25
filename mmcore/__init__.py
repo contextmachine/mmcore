3dcb29704cf711f1d969e5e5b8179a44d63cc5fd
sth-v <aa@contextmachine.ru>
2024-03-26 01:14:38 +0300

    vers fix

--- mmcore/__init__.py
+++ mmcore/__init__.py
@@ -1,16 +1,22 @@
-#Generated with ./bin/upd-version.py
 from pathlib import Path
-
 from mmcore.utils.env import load_dotenv_from_path, load_dotenv_from_stream
+load_dotenv_from_path('.env')
+TOLERANCE = 1e-06
 
-load_dotenv_from_path(".env")
+class VersionInfo(str):
 
-TOLERANCE = 0.000_001
+    def __new__(cls, val):
+        self = super().__new__(cls)
+        self._value = val
+        return self
 
+    def __str__(self):
+        return str.__str__(self._value)
 
-def __version__():
-    import toml
-    with open((Path(__file__).parent/Path("../pyproject.toml")).resolve()) as f:
-        data=toml.load(f)
+    def __repr__(self):
+        return str.__str__(self._value)
 
-    return  data['tool']['poetry']['version']
+    def __call__(self):
+        return self._value
+
+__version__ = VersionInfo('0.23.12')
