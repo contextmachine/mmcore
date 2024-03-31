324fb7707ee0d5aacb9c9a8a5759a98d5ca8e7bb
sth-v <aa@contextmachine.ru>
2024-03-31 10:06:08 +0300

    build fix

--- mmcore/__init__.py
+++ mmcore/__init__.py
@@ -1,10 +1,9 @@
 from mmcore.utils.env import load_dotenv_from_path, load_dotenv_from_stream
-
-load_dotenv_from_path(".env")
+load_dotenv_from_path('.env')
 TOLERANCE = 1e-06
 
-
 class VersionInfo(str):
+
     def __new__(cls, val):
         self = super().__new__(cls)
         self._value = val
@@ -18,6 +17,4 @@ class VersionInfo(str):
 
     def __call__(self):
         return self._value
-
-
-__version__ = VersionInfo("0.23.14")
+__version__ = VersionInfo('0.23.15')
