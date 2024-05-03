cbb050f434672b2813e0d2a5550279faf2ef1804
sth-v <aa@contextmachine.ru>
2024-05-03 05:58:20 +0300

    Refactor evaluate methods and add performance improvements
    Significant changes have been made to the evaluation methods within the 'bspline' and 'deboor' files. Optimized methods 'evaluate_nurbs' and 'evaluate_nurbs_multi' have been added to the 'deboor' file to handle single and multiple evaluations respectively, improving performance. 'bspline' file has been refactored to use these new methods for a more efficient execution. In addition, a new function 'step' has been introduced in the 'arc_length' file.

--- mmcore/geom/curves/deboor.pyx
+++ mmcore/geom/curves/deboor.pyx
@@ -1,4 +1,9 @@
 cimport cython
+
+cimport numpy as np
+import numpy as np
+ctypedef struct xyz:
+    double x,y,z;
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cdef double cdeboor(double[:] knots, double t, int i, int k) noexcept nogil:
@@ -25,7 +30,52 @@ cdef double cdeboor(double[:] knots, double t, int i, int k) noexcept nogil:
     return c1 + c2
 
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef xyz cevaluate_nurbs(double t, double[:,:] cpts, double[:] knots, double[:] weights, int degree  ):
+    cdef Py_ssize_t n=cpts.shape[0]
+    cdef double b;
+    cdef double crd;
+    cdef double sum_of_weights =0.;
+
+    cdef xyz result=xyz(0.,0.,0.);
+    for i in range(n):
+        b = cdeboor(knots, t, i, degree)* weights[i]
+        result.x += b * cpts[i][0]
+        result.y  += b * cpts[i][1]
+        result.z  += b *  cpts[i][2]
+        sum_of_weights += b
+    result.x = result.x / sum_of_weights
+    result.y = result.y / sum_of_weights
+    result.z = result.z / sum_of_weights
+    return result
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef void cevaluate_nurbs_multi( double[:] t,double[:,:] cpts, double[:] knots, double[:] weights, int degree,double[:,:] result) :
+
+    cdef xyz pt;
+
+    for i in range(t.shape[0]):
+        pt=cevaluate_nurbs(t[i],cpts,knots,weights,degree)
+        result[i][0]=pt.x
+        result[i][1] = pt.y
+        result[i][2] = pt.z
+
+
 def deboor(double[:] knots, double t, int i, int k)   :
     cdef double d=cdeboor(knots,t,i,k)
     return d
 
+def evaluate_nurbs(double t, double[:,:] cpts, double[:] knots, double[:] weights, int degree )   :
+    cdef double[:] result=np.empty((3,))
+    cdef xyz pt=cevaluate_nurbs(t,cpts,knots,weights,degree)
+    result[0] = pt.x
+    result[1] = pt.y
+    result[2] = pt.z
+
+    return  result
+def evaluate_nurbs_multi(double[:] t, double[:,:] cpts, double[:] knots, double[:] weights, int degree )   :
+    cdef double[:,:] result=np.empty((t.shape[0],3))
+    cevaluate_nurbs_multi(t,cpts,knots,weights,degree,result)
+    return  result
