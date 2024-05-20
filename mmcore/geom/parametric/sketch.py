fbb26b7da39ef2b3e401fa5c86b1eae3476ce8db
sth-v <aa@contextmachine.ru>
2024-05-20 14:24:21 +0300

    fix imports

--- mmcore/geom/parametric/sketch.py
+++ mmcore/geom/parametric/sketch.py
@@ -15,8 +15,7 @@ import numpy as np
 from scipy.optimize import minimize
 from scipy.spatial.distance import euclidean
 
-import mmcore.api.plane
-import mmcore.api.surface
+
 from mmcore.collections import DCLL, DoublyLinkedList
 from mmcore.collections.multi_description import EntityCollection
 from mmcore.geom.materials import ColorRGB
