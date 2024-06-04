16fbf6e51ece2c4287b6e618350e47e60b292a92
sth-v <aa@contextmachine.ru>
2024-06-04 04:00:29 +0300

    changes in reqs

--- mmcore/__init__.py
+++ mmcore/__init__.py
@@ -1,8 +1,4 @@
-try:
-    from importlib.metadata import version
-except ImportError:
-    # For Python<3.8, use importlib-metadata package
-    # noinspection PyUnresolvedReferences
-    from importlib_metadata import version
+from importlib.metadata import version
+
 
 __version__ = version("your-package-name")
