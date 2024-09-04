315251e3e704e4590c3bd7930f1586c35b403bb8
sth-v <aa@contextmachine.ru>
2024-09-04 07:13:48 +0300

    Add libpython3-dev to apt install command
    The script now installs libpython3-dev as part of the dependencies. This ensures that all Python development headers are available for the build process.

--- docker-build-step1.sh
+++ docker-build-step1.sh
@@ -1,7 +1,7 @@
 mkdir /tmp/build-python
 cd /tmp/build-python
 apt update -y
-apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev libpython3-dev
 wget https://www.python.org/ftp/python/3.12.2/Python-3.12.2.tgz
 tar -xf Python-3.12.2.tgz
 cd Python-3.12.*/
