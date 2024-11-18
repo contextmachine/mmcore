0f2751b2e86333821b0ece4f40a67cdc12fb9c66
sth-v <sthv.developer@gmail.com>
2024-11-19 00:55:32 +0300

    Refactor code and update project configuration
    1. mmcore/topo/mesh/triangle/tri.py
    Refactor `prepare_args` function for better readability
    
    Removed the usage of `Unpack` from `typing_extensions` and enhanced the `prepare_args` function by using the unpacking (*) operator directly. This change addresses the readability and compatibility of the code by simplifying and modernizing the approach for unpacking boundaries and holes.
    
    2. pyproject.toml
    Bump the project version to 0.49.1
    
    Increase the version from 0.49.0 to 0.49.1 to reflect the new changes and updates in the project. This change ensures proper versioning and helps in tracking the updates made to the project.
    
    3. mmcore/numeric/newthon/constrained.py
    Add new `constrained` module for Newton's and SQP methods
    
    Introduced a new file `constrained.py` under the `newthon` module which includes multiple functions for Newton's methods and Sequential Quadratic Programming (SQP). These methods are essential for solving constrained optimization problems. The file defines Newton's method for solving systems of equations, computes gradients, Hessians, and implements SQP for optimization. The addition enhances the numerical capabilities of the project.
    
    4. mmcore/compat/step/step_writer.py
    Clean up and refactor `step_writer.py` for better maintainability
    
    Removed unused imports, redundant class definitions, and commented-out code, improving the readability and maintainability of the file. Updated the STEP file header to include `mmcore` version information dynamically. Additionally, moved constants to a more appropriate location, ensuring that the codebase is cleaner and follows a more modular approach.

--- mmcore/numeric/newthon/__init__.py
+++ mmcore/numeric/newthon/__init__.py
@@ -1,5 +1,86 @@
 import numpy as np
 
 import warnings
-from mmcore.numeric.fdm import newtons_method
-__all__ = ['newtons_method']
+from mmcore.numeric.fdm import newtons_method,hessian,gradient
+__all__ = ['newtons_method', 'newtons_method_with_constraints']
+
+from mmcore.numeric.vectors import scalar_norm, scalar_dot
+
+
+def newtons_method_with_constraints(f, constraint, initial_point, tol=0.00001, max_iter=100, no_warn=False, full_return=False, grad=None, hess=None, lagrange_multiplier=1.0):
+    """
+    Apply Newton's method to find the root of a function with constraints.
+    The same powerful newton converging in a few iterations.
+
+    :param f: The function for which the root is to be found.
+    :param constraint: The constraint function.
+    :param initial_point: The initial point for the iteration.
+    :param tol: Tolerance for the stopping criterion. Default is DEFAULT_H.
+    :param max_iter: Maximum number of iterations. Default is 100.
+    :param no_warn: If True, suppress warnings. Default is False.
+    :param full_return: If True, return all intermediate variables. Default is False.
+    :param grad: The gradient of the function. If None, compute the gradient using the gradient function.
+    :param hess: The Hessian of the function. If None, compute the Hessian using the hessian function.
+    :param lagrange_multiplier: The initial value of the Lagrange multiplier. Default is 1.0.
+    :return: The root of the function if found, None otherwise.
+    """
+    point = np.asarray(initial_point)
+    lagrange_multiplier = lagrange_multiplier
+    H_inv = None
+    H = None
+    if grad is None:
+        _grad = lambda x: gradient(f, x)
+    else:
+        _grad = grad
+    if hess is None:
+        hess = lambda x: hessian(f, x)
+    else:
+        hess = hess
+
+    for _ in range(max_iter):
+        grad_f = _grad(point)
+        grad_g = gradient(constraint, point)
+        H_f = hess(point)
+        H_g = hessian(constraint,point)
+
+        try:
+            H_inv_f = np.linalg.inv(H_f)
+        except np.linalg.LinAlgError:
+            if not no_warn:
+                warnings.warn(f"Hessian of f is singular at the point {point}")
+            break
+
+        try:
+            H_inv_g = np.linalg.inv(H_g)
+        except np.linalg.LinAlgError:
+            if not no_warn:
+                warnings.warn(f"Hessian of g is singular at the point {point}")
+            break
+
+        grad_F = grad_f - lagrange_multiplier * grad_g
+        H_F = H_f + lagrange_multiplier * H_g
+
+        step = H_inv_f @ grad_F
+
+        new_point = point - step
+
+
+        if scalar_dot(step,step) < tol:
+            if full_return:
+                return new_point, grad_F, H_F, H_inv_f, _
+            return new_point
+        if scalar_norm(point-new_point)<tol:
+            if full_return:
+                return new_point, grad_F, H_F, H_inv_f, _
+            return new_point
+        print(max_iter, new_point,point)
+        point = new_point
+
+    if not no_warn:
+
+        warnings.warn(f"Iteration limit {max_iter} at {new_point}, {scalar_dot(new_point-point,new_point-point)}")
+    if full_return:
+
+        return None, grad_F, H_F, H_inv_f, max_iter
+
+    return point
