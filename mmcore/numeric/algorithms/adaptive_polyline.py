997e52f005c8edc2cbba62be22a8b3d2095a405b
sth-v <sthv.developer@gmail.com>
2025-03-16 22:54:43 +0300

    Add condition to handle degenerate control points in adaptive_polyline
    1. mmcore/numeric/algorithms/adaptive_polyline.py
    Improve adaptive polyline generation with a check for degenerate control points.
    
    The adaptive_polyline function is updated to include a condition that checks whether the norm of the curve control points relative to the Greville abscissae is approximately zero, using `np.allclose`. This ensures that degenerate curves, where all control points collapse to a single point (or are poorly defined), are gracefully handled by directly returning unique knot evaluations.
    This change prevents potential issues in subsequent computations for such cases, enhancing the function's robustness and alignment with edge case handling. Additionally, an unused newline was removed for better code clarity.

--- mmcore/numeric/algorithms/adaptive_polyline.py
+++ mmcore/numeric/algorithms/adaptive_polyline.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from mmcore.geom.nurbs import NURBSCurve
-from mmcore.numeric.vectors import norm
+from mmcore.numeric.vectors import norm,dot
 
 def chord_length(R, h):
     return 2 * np.sqrt(2 * R * h - (h * h))
@@ -9,9 +9,33 @@ def chord_length(R, h):
 
 def adaptive_polyline(curve: NURBSCurve, tol:float):
     greville_abscissae_points=np.array(curve.evaluate_multi(curve.greville_abscissae))
-    if curve.degree < 2 or np.allclose(np.array(norm(np.asarray(curve.control_points)-greville_abscissae_points)),0):
+    if curve.degree < 2 :
 
         return greville_abscissae_points, curve.greville_abscissae
+    normals=np.asarray(curve.control_points) - greville_abscissae_points
+    dn=np.array(norm(normals))
+    if np.allclose(dn,0):
+        return greville_abscissae_points, curve.greville_abscissae
+    _res=[]
+    for i in range(normals.shape[0]):
+        if np.isclose(dn[i],0):
+            continue
+
+        n=normals[i]
+        n/=dn[i]
+        t=curve.greville_abscissae[i]
+        tangent=np.array(curve.derivative( t))
+        tangent/=np.linalg.norm(tangent
+                                )
+        _res.append(1.-np.abs(np.dot(n,tangent)))
+
+
+
+    if np.allclose(_res,0):
+        return greville_abscissae_points, curve.greville_abscissae
+
+
+
 
     params = [*tuple(curve.interval())]
     points = [curve.evaluate(params[0]), curve.evaluate(params[1])]
@@ -33,9 +57,9 @@ def adaptive_polyline(curve: NURBSCurve, tol:float):
             if (L >= segment_length) and (np.linalg.norm(p_curve_mid - p_line_mid) < tol):
                 return
         # Subdivide further
-        subdivide(t0, t_mid, p0, p_curve_mid, tol, points_list)
+        subdivide(curve,t0, t_mid, p0, p_curve_mid, tol, points_list)
         points_list.append((t_mid, p_curve_mid))
-        subdivide(t_mid, t1, p_curve_mid, p1, tol, points_list)
+        subdivide(curve,t_mid, t1, p_curve_mid, p1, tol, points_list)
 
     points_list = []
     subdivide(curve,params[0], params[1], points[0], points[1], tol, points_list)
