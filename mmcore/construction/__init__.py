6998e9fe8832eb87a760d3e1b43e77561d916627
sth-v <sthv.developer@gmail.com>
2025-12-19 14:16:34 +0300

    Add `circular_cone` and `nurbs_surface` utilities, refactor imports:
    - Introduce `circular_cone` function extending cone modeling capabilities.
    - Add `nurbs_surface` for NURBS surface construction with customizable parameters.
    - Refactor imports in `__init__.py` to include new utilities and improve consistency.
    - Update `nurbs_curve` to use `from_homogeneous_1d` for enhanced rational handling.

--- mmcore/construction/__init__.py
+++ mmcore/construction/__init__.py
@@ -4,21 +4,24 @@ from mmcore.geom._nurbs_construct import circle
 from mmcore.geom._nurbs_eval import NURBSCurveTuple,NURBSSurfaceTuple
 from ._ruled import ruled
 from ._torus import torus
-from ._cone import elliptical_cone
+from ._cone import circular_cone,elliptical_cone
 from ._revolved import revolved
 from ._cylinder import cylinder_surface_2pt,cylinder_surface
 from ._sweep import sweep1
 from ._curve import nurbs_curve
+from ._surface import nurbs_surface
 __all__=[
     'ruled',
     'revolved',
     'torus',
+    "circular_cone",
     'elliptical_cone',
     'circle',
     'cylinder_surface',
     'cylinder_surface_2pt',
     'sweep1',
     'nurbs_curve',
+    'nurbs_surface',
     'NURBSCurveTuple',
     'NURBSSurfaceTuple'
 ]
