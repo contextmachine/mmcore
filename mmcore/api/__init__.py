cc45ef81386c416387becbc6111502a494987070
sth-v <sthv.developer@gmail.com>
2026-06-13 14:16:15 +0300

    Remove `mmcore/api` directory and associated modules to clean up deprecated and unused API components.

--- mmcore/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from ._typing import *
-
-from .base import *
-from .vectors import *
-from .bbox import *
-from .curves import *
-from .surface import *
-from .enums import *
--- mmcore/api/_typing.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-import sys
-
-Self = object
-import typing
-
-if sys.version_info.minor < 11:
-    typing.Self = object
-from typing import *
